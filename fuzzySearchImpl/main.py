109d4a3f520adae16daaa27668a4bcbe22c48bda
ZNL17 <ziyulin17@gmail.com>
2025-11-22 23:54:29 +0100

    Levenshtein distanz implementiert

--- fuzzySearchImpl/main.py
+++ fuzzySearchImpl/main.py
@@ -1,1 +1,17 @@
-#
+import sys
+def levdist(s: str, t:str):
+    if s == '':
+        return len(t)
+    if t == '':
+        return len(s)
+    if s[-1] == t[-1]:
+        cost = 0
+    else:
+        cost = 1
+    dist = min([
+        levdist(s[:-1] ,t) + 1,
+        levdist(s, t[:-1]) + 1,
+        levdist(s[:-1], t[:-1]) + cost])
+    return dist
+print(levdist(sys.argv[1], sys.argv[2]))
+
